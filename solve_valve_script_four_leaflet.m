697adc06da88e29e8915c840389daac33b1cab99
adkaiser <adkaiser@lbl.gov>
2015-11-18 17:33:17 -0500

    updated four leaflet script to be able to accommodate two leaflets by just changing a parameter

--- solve_valve_script_four_leaflet.m
+++ solve_valve_script_four_leaflet.m
@@ -9,15 +9,19 @@ if restart_number ~= 0
 else 
 
     % symmetric for now 
-    commissure_angle = pi/3; 
+    commissure_angle = 0.0; 
+    
+    % note that if extra posterior is equal to commissure angle
+    % then the poster leaflet has angle pi 
+    extra_posterior = pi/6; 
     
     a = 1; 
     r = 1.5;
     h = 2; 
     N = 32; 
 
-    min_angle_posterior = -(pi/2 - commissure_angle/2); 
-    max_angle_posterior =  (pi/2 - commissure_angle/2); 
+    min_angle_posterior = -(pi/2 - commissure_angle/2 + extra_posterior/2); 
+    max_angle_posterior =  (pi/2 - commissure_angle/2 + extra_posterior/2); 
 
     filter_params_posterior.a = a; 
     filter_params_posterior.r = r; 
@@ -32,7 +36,7 @@ else
     X = R; 
     alpha     =  1.0; % spring constants in two directions 
     beta      =  1.0;
-    p_0       = -30.0; 
+    p_0       = -10.0; 
     ref_frac  =  0.5; 
 
     params_posterior = pack_params(X,alpha,beta,N,p_0,R,ref_frac); 
@@ -43,7 +47,7 @@ else
     'initial difference equation norm'
     err = total_global_err(params_posterior, filter_params_posterior)
     
-    tol_global = 1e-12; 
+    tol_global = 1e-11; 
     max_it_global = 100000; 
     
     plot_and_save_freq = 10; 
@@ -53,8 +57,8 @@ else
     
     
     % anterior 
-    min_angle_anterior = -(pi/2 - commissure_angle/2); 
-    max_angle_anterior =  (pi/2 - commissure_angle/2); 
+    min_angle_anterior = -(pi/2 - commissure_angle/2 - extra_posterior/2); 
+    max_angle_anterior =  (pi/2 - commissure_angle/2 - extra_posterior/2); 
 
     filter_params_anterior.a = a; 
     filter_params_anterior.r = r; 
@@ -80,58 +84,61 @@ else
     
     
     % left commissural leaflet
-    % total hack on dimensions here for now 
-    N_left = floor(N/2) + 1; 
-    center = -pi/2; 
-    min_angle_left = center - commissure_angle/2; 
-    max_angle_left = center + commissure_angle/2;
-    left = true; 
-    
-    filter_params_left.a = a; 
-    filter_params_left.r = r; 
-    filter_params_left.h = h;
-    filter_params_left.N = N_left;
-    filter_params_left.min_angle = min_angle_left;
-    filter_params_left.max_angle = max_angle_left;
-    
-    R = build_reference_surface_commissure(filter_params_left, left);
-    X = R; 
-    params_left = pack_params(X,alpha,beta,N_left,p_0,R,ref_frac); 
-    
-    fig = surf_plot_commissure(params_left, filter_params_left, left); 
-    title('Reference configuration of surface'); 
-    
-    'initial difference equation norm'
-    err = total_global_err_commissure(params_left, filter_params_left, left);
-    
-    err_over_time_left = zeros(max_it_global,1); 
-    
-    % right commissural leaflet
-    % total hack on dimensions here for now 
-    N_right = floor(N/2) + 1; 
-    center = pi/2; 
-    min_angle_right = center - commissure_angle/2; 
-    max_angle_right = center + commissure_angle/2;
-    left = false; 
-    
-    filter_params_right.a = a; 
-    filter_params_right.r = r; 
-    filter_params_right.h = h;
-    filter_params_right.N = N_right;
-    filter_params_right.min_angle = min_angle_right;
-    filter_params_right.max_angle = max_angle_right;
-    
-    R = build_reference_surface_commissure(filter_params_right, left);
-    X = R; 
-    params_right = pack_params(X,alpha,beta,N_right,p_0,R,ref_frac); 
-    
-    fig = surf_plot_commissure(params_right, filter_params_right, left); 
-    title('Reference configuration of surface'); 
-    
-    'initial difference equation norm'
-    err = total_global_err_commissure(params_right, filter_params_right, left);
-    
-    err_over_time_right = zeros(max_it_global,1); 
+    % only fill in if commissural leaflet has nonzero angle 
+    if commissure_angle > 0.0
+        % total hack on dimensions here for now     
+        N_left = floor(N/2) + 1; 
+        center = -(pi/2 - extra_posterior/2); 
+        min_angle_left = center - commissure_angle/2; 
+        max_angle_left = center + commissure_angle/2;
+        left = true; 
+
+        filter_params_left.a = a; 
+        filter_params_left.r = r; 
+        filter_params_left.h = h;
+        filter_params_left.N = N_left;
+        filter_params_left.min_angle = min_angle_left;
+        filter_params_left.max_angle = max_angle_left;
+
+        R = build_reference_surface_commissure(filter_params_left, left);
+        X = R; 
+        params_left = pack_params(X,alpha,beta,N_left,p_0,R,ref_frac); 
+
+        fig = surf_plot_commissure(params_left, filter_params_left, left); 
+        title('Reference configuration of surface'); 
+
+        'initial difference equation norm'
+        err_left = total_global_err_commissure(params_left, filter_params_left, left)
+
+        err_over_time_left = zeros(max_it_global,1); 
+
+        % right commissural leaflet
+        % total hack on dimensions here for now 
+        N_right = floor(N/2) + 1; 
+        center = (pi/2 - extra_posterior/2); 
+        min_angle_right = center - commissure_angle/2; 
+        max_angle_right = center + commissure_angle/2;
+        left = false; 
+
+        filter_params_right.a = a; 
+        filter_params_right.r = r; 
+        filter_params_right.h = h;
+        filter_params_right.N = N_right;
+        filter_params_right.min_angle = min_angle_right;
+        filter_params_right.max_angle = max_angle_right;
+
+        R = build_reference_surface_commissure(filter_params_right, left);
+        X = R; 
+        params_right = pack_params(X,alpha,beta,N_right,p_0,R,ref_frac); 
+
+        fig = surf_plot_commissure(params_right, filter_params_right, left); 
+        title('Reference configuration of surface'); 
+
+        'initial difference equation norm'
+        err_right = total_global_err_commissure(params_right, filter_params_right, left)
+
+        err_over_time_right = zeros(max_it_global,1); 
+    end 
 end 
 
 
@@ -163,33 +170,34 @@ else
     disp('Global solve failed')
 end 
 
-left = true; 
-[params_left pass err_over_time_left it] = solve_commissure_leaflet(params_left, filter_params_left, tol_global, max_it_global, plot_and_save_freq, start_it, err_over_time_left, left); 
+if commissure_angle > 0.0
+    left = true; 
+    [params_left pass err_over_time_left it] = solve_commissure_leaflet(params_left, filter_params_left, tol_global, max_it_global, plot_and_save_freq, start_it, err_over_time_left, left); 
 
 
-if pass 
-    disp('Global solve passed')
-else 
-    disp('Global solve failed')
+    if pass 
+        disp('Global solve passed')
+    else 
+        disp('Global solve failed')
+    end 
+
+    'difference equation norm left'
+    err_left = total_global_err_commissure(params_left, filter_params_left, left)
+
+
+    left = false; 
+    [params_right pass err_over_time_right it] = solve_commissure_leaflet(params_right, filter_params_right, tol_global, max_it_global, plot_and_save_freq, start_it, err_over_time_right, left); 
+
+
+    if pass 
+        disp('Global solve passed')
+    else 
+        disp('Global solve failed')
+    end 
+
+    'difference equation norm right'
+    err_right = total_global_err_commissure(params_right, filter_params_right, left)
 end 
-
-'difference equation norm left'
-err_left = total_global_err_commissure(params_left, filter_params_left, left)
-
-
-left = false; 
-[params_right pass err_over_time_right it] = solve_commissure_leaflet(params_right, filter_params_right, tol_global, max_it_global, plot_and_save_freq, start_it, err_over_time_right, left); 
-
-
-if pass 
-    disp('Global solve passed')
-else 
-    disp('Global solve failed')
-end 
-
-'difference equation norm right'
-err_right = total_global_err_commissure(params_right, filter_params_right, left)
-
 
 
 fig = figure; 
@@ -197,11 +205,13 @@ fig = surf_plot(params_posterior, filter_params_posterior, fig);
 hold on 
 fig = surf_plot(params_anterior, filter_params_anterior, fig);
 
-left = true; 
-fig = surf_plot_commissure(params_left, filter_params_left, left, fig); 
+if commissure_angle > 0.0
+    left = true; 
+    fig = surf_plot_commissure(params_left, filter_params_left, left, fig); 
 
-left = false;
-fig = surf_plot_commissure(params_right, filter_params_right, left, fig); 
+    left = false;
+    fig = surf_plot_commissure(params_right, filter_params_right, left, fig); 
+end 
 
 title(sprintf('Final time difference equation solution, p = %f, ref_frac = %f' , params_posterior.p_0, params_posterior.ref_frac )); 
 name = sprintf('surf_p_%f', params_posterior.p_0); 
@@ -213,10 +223,18 @@ fig = figure;
 semilogy(err_over_time_posterior, '*-'); 
 hold on 
 semilogy(err_over_time_anterior, 's-'); 
-semilogy(err_over_time_left, 'o-'); 
-semilogy(err_over_time_right, '+-'); 
 
-legend('posterior', 'anterior', 'left', 'right', 'location', 'SouthWest'); 
+if commissure_angle > 0.0
+    semilogy(err_over_time_left, 'o-'); 
+    semilogy(err_over_time_right, '+-'); 
+end 
+
+if commissure_angle > 0.0
+    legend('posterior', 'anterior', 'left', 'right', 'location', 'SouthWest'); 
+else
+    legend('posterior', 'anterior', 'location', 'SouthWest'); 
+end 
+
 xlabel('iteration')
 ylabel('log(err)')
 
