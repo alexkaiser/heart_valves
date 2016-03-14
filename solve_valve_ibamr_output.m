63b15bd9c45a97d219da505f6116a356f7f9c016
alexkaiser <adkaiser@gmail.com>
2016-03-13 22:04:50 -0400

    this version passes with n=128

--- solve_valve_ibamr_output.m
+++ solve_valve_ibamr_output.m
@@ -19,7 +19,7 @@ chordae_tree = true;
 a = 1; 
 r = 1.5;
 h = 3; 
-N = 64; 
+N = 128; 
 
 
 min_angle_posterior = -(pi/2 - commissure_angle/2 + extra_posterior/2 + overlap/2); 
@@ -65,7 +65,7 @@ end
 
 
 tol_global = 1e-11; 
-max_it_global = 20; 
+max_it_global = 30; 
 
 plot_and_save_freq = 10; 
 start_it = 0; 
