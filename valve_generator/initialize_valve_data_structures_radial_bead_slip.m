03852982ccf4b8c9480a688b524e619244c28b3d
alexkaiser <adkaiser@gmail.com>
2017-04-25 15:49:05 -0400

    turned pressure up on zero extra hoop version, diastolic geometry

--- valve_generator/initialize_valve_data_structures_radial_bead_slip.m
+++ valve_generator/initialize_valve_data_structures_radial_bead_slip.m
@@ -166,7 +166,7 @@ ring_to_ring_range = 0;
 
 
 
-sytole_skeleton = true; 
+sytole_skeleton = false; 
 if sytole_skeleton 
     valve.skeleton = valve_points_ct_systole(); 
 else 
@@ -195,7 +195,7 @@ valve.total_angle_dip = pi;
 
 % pressure / tension coefficient ratio
 % this tension coefficient is the maximum tension that a fiber can support
-valve.pressure_tension_ratio = 0.035; % 0.11 * 0.975; 
+valve.pressure_tension_ratio = 0.0825; % 0.11 * 0.975; 
 
 
 % base constant for tensions, derived quantity 
@@ -208,7 +208,7 @@ beta     = 1.0 * valve.tension_base;  % radial
 
 
 % places this many periodic rings above 
-n_rings_periodic = max(1,N/32); 
+n_rings_periodic = 0; %åmax(1,N/32); 
 
 
 parameter_values = 4; 
