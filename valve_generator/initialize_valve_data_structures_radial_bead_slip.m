4c8fc9e0b1176d4a91e2eac04361eed3d46db249
alexkaiser <adkaiser@gmail.com>
2017-07-08 01:04:15 -0400

    cross link multiplier 1e6

--- valve_generator/initialize_valve_data_structures_radial_bead_slip.m
+++ valve_generator/initialize_valve_data_structures_radial_bead_slip.m
@@ -627,7 +627,7 @@ valve.eta_papillary_unscaled = valve.target_papillary_unscaled/500;
 % if nonzero, linear springs of rest length with spacing between the layers 
 % are placed with this value 
 % final formula is multiplied by valve.tension_base / (valve.N^2); 
-valve.kappa_cross_layer_multipler = 1e5; 
+valve.kappa_cross_layer_multipler = 1e6; 
 
 % Approximate Lagrangian mesh spacing at ring 
 % Used for later splitting of springs 
