97e7188487a04719eb54e09eff53eb72eaa4af7b
alexkaiser <adkaiser@gmail.com>
2019-03-12 20:05:35 -0700

    Revert "hard cutoff bc aortic (again) here"
    This reverts commit 5636530faa297c1c3f2620840603bf528786533d.

--- FeedbackForcer.cpp
+++ FeedbackForcer.cpp
@@ -226,6 +226,12 @@ FeedbackForcer::setDataOnPatch(const int data_idx,
                         mask = 1.0;
                     }
 
+                    // if aortic valve should be closed, damp to zero 
+                    const int idx = d_circ_model_with_lv->d_current_idx_series; 
+                    if (d_circ_model_with_lv->d_fourier_aorta->values[idx] > d_circ_model_with_lv->d_fourier_ventricle->values[idx]){
+                        mask = 1.0; 
+                    } 
+
                 }
 
                 if (component == axis && (in_atrium)){
