5b2956df3d66a157b30d72ca44026b3f64771118
alexkaiser <adkaiser@gmail.com>
2019-07-08 23:44:39 -0700

    little to no self intersection at these pressures

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -128,7 +128,7 @@ valve.p_physical = 100 * MMHG_TO_CGS;
 % Pressure on each leaflet is constant, negative since normal is outward facing 
 p_0 = -valve.p_physical; 
 
-valve.p_final = 0.01 * MMHG_TO_CGS; 
+valve.p_final = 0.001 * MMHG_TO_CGS; 
 
 % valve.dip_anterior_systole = true; 
 % valve.r_dip = 0.75; 
