e2de519c1676b4ed7d1a70bb34b35f5b4acc7001
alexkaiser <adkaiser@gmail.com>
2019-08-14 01:21:02 -0700

    circ strain .1 k_bend 0

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -97,7 +97,7 @@ valve.X_config_is_reference = true;
 % spacing is always half a mesh width 
 valve.num_copies = 3; 
 
-valve.normal_thicken = true; 
+valve.normal_thicken = false; 
 % nominal aortic valve thickness
 valve.normal_thickness = 0.044; 
 
@@ -121,7 +121,7 @@ valve.collagen_constitutive_circ = true;
 valve.collagen_constitutive_rad  = false; 
 
 % Constant strain of pressurized configuration 
-valve.strain_circ = .12; 
+valve.strain_circ = .10; 
 valve.strain_rad  = .01; 
 
 % physical units create a scalar multiple of the old 
@@ -199,7 +199,7 @@ valve.eta_papillary_unscaled = 0.0; valve.target_papillary_unscaled/500;
 % final formula is multiplied by valve.tension_base  
 valve.kappa_cross_layer_multipler = 1e4 / 256^2; 
 
-valve.k_bend_radial = 1e2 * 192/N; 
+valve.k_bend_radial = 0; 1e2 * 192/N; 
 % valve.k_bend_radial_free_edge = 1e5 * 192/N; 
 % valve.k_bend_radial_free_edge_percentage = .4; 
 valve.k_bend_circ   = 0; 
