01606759a0c133d497c8f6dedfb7cacc83fdf738
alexkaiser <adkaiser@gmail.com>
2019-11-22 16:22:08 -0800

    radial strain to pt24, retuned height

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -128,7 +128,7 @@ valve.collagen_constitutive_rad  = false;
 
 % Constant strain of pressurized configuration 
 valve.strain_circ = .12; 
-valve.strain_rad  = .20; 
+valve.strain_rad  = .24; 
 
 % physical units create a scalar multiple of the old 
 % this multiple is large number, so we want to scale the old tolerance accordingly 
@@ -178,11 +178,11 @@ tension_coeffs.dec_tension_coeff_base = 20.0;
 
 % max tensions in leaflets 
 tension_coeffs.alpha = 1.6;   % circumferential 
-tension_coeffs.beta  = 0.041;   % radial
+tension_coeffs.beta  = 0.045;   % radial
 
 % decreasing tension coefficients 
 tension_coeffs.c_circ_dec       = 4.0;  % circumferential 
-tension_coeffs.c_rad_dec        = 0.58;  % radial
+tension_coeffs.c_rad_dec        = 0.65;  % radial
 
 % scaling for target points 
 % note that this does not include copies 
