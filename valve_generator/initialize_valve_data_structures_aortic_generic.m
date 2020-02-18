9b417685bdd24c83b337f7b2c74f1672ce315aa7
alexkaiser <adkaiser@gmail.com>
2020-02-17 16:28:59 -0800

    mitral to old nonlinear law, radial to new nonlienar law, basic bending

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -123,8 +123,8 @@ valve.dirichlet_free_edge_with_ref_only = true;
 
 % Uses collagen spring function implemented in IBAMR 
 % Spring constants are different here 
-valve.collagen_constitutive_circ = 'aortic_circ'; 
-valve.collagen_constitutive_rad  = false; %'aortic_rad'; 
+valve.collagen_constitutive_circ = true; %'aortic_circ'; 
+valve.collagen_constitutive_rad  = 'aortic_rad'; 
 
 % Constant strain of pressurized configuration 
 valve.strain_circ = .15; 
