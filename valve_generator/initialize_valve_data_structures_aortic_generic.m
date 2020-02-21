242c6ff63afc3b2f4b0a22942cd71246662cf794
alexkaiser <adkaiser@gmail.com>
2020-02-21 15:55:14 -0800

    bending back to default

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -210,10 +210,10 @@ valve.eta_papillary_unscaled = 0.0; valve.target_papillary_unscaled/500;
 % final formula is multiplied by valve.tension_base  
 valve.kappa_cross_layer_multipler = 1e4 / 256^2; 
 
-valve.k_bend_radial = 1e4 * 192/N; 
+valve.k_bend_radial = 1e2 * 192/N; 
 valve.k_bend_radial_free_edge = 1e5 * 192/N; 
 valve.k_bend_radial_free_edge_percentage = .4; 
-valve.k_bend_circ   = 1e4; 
+valve.k_bend_circ   = 0; 
 
 % Approximate Lagrangian mesh spacing at ring 
 % Used for later splitting of springs 
