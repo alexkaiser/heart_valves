3a645126ee76f21a1d9117c898cedb01225aee44
alexkaiser <adkaiser@gmail.com>
2020-02-24 11:59:23 -0800

    strain at pt2, 4mm extra free edge length

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -187,8 +187,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 4.6;  % circumferential 
-tension_coeffs.c_rad_dec        = 4.6;  % radial
+tension_coeffs.c_circ_dec       = 4.9;  % circumferential 
+tension_coeffs.c_rad_dec        = 6.3;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.5;        
 
