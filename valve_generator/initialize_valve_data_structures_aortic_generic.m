d30e2cd881ddd13c86057e256a0284bddb22c005
alexkaiser <adkaiser@gmail.com>
2020-03-01 21:52:32 -0800

    at new targets

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -187,8 +187,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 4.7;  % circumferential 
-tension_coeffs.c_rad_dec        = 1.4;  % radial
+tension_coeffs.c_circ_dec       = 4.35;  % circumferential 
+tension_coeffs.c_rad_dec        = 1.1;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.5;        
 
