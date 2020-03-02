8d56b48784b06e8fcb52d0ee4cd80f7cf72080ef
alexkaiser <adkaiser@gmail.com>
2020-03-01 22:09:47 -0800

    0mm extra circ 2mm extra radial

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -187,7 +187,7 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 4.3;  % circumferential 
+tension_coeffs.c_circ_dec       = 4.1;  % circumferential 
 tension_coeffs.c_rad_dec        = 1.56;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.5;        
