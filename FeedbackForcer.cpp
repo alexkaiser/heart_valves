96f61d7bce5e6c8911fdc34c0f8cc3f6d701e956
alexkaiser <adkaiser@gmail.com>
2020-03-28 17:54:42 -0700

    damping outside of relevant part of domain to try and quiet down extra junk in flow, all edges to zero

--- FeedbackForcer.cpp
+++ FeedbackForcer.cpp
@@ -225,7 +225,7 @@ FeedbackForcer::setDataOnPatch(const int data_idx,
                     }
 
                     // no bdry stab unless one of the conditionals is met 
-                    double mask = 0.0;
+                    double mask = 1.0;
 
                     double U_goal = 0.0; 
 
