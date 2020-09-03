395b38cb9417468394040b553b1503087a113f37
alexkaiser <adkaiser@gmail.com>
2020-09-02 19:04:48 -0700

    pinched in scaffold

--- valve_generator/initialize_valve_data_structures_pulm_mri_box.m
+++ valve_generator/initialize_valve_data_structures_pulm_mri_box.m
@@ -259,7 +259,7 @@ valve.ds = dx/2; %2*pi*valve.skeleton.r / N;
 if mri_box
     thickness_cylinder = 0.3; 
     valve.n_layers_cylinder = ceil(thickness_cylinder/valve.ds) + 1; 
-    valve.z_max_cylinder = @(theta)  0.28*ones(size(theta))  + (1.095 - 0.28)*0.5*(cos(3*theta)+1); 
+    valve.z_max_cylinder = @(theta)  0.28*ones(size(theta))  + (1.095 - 0.28)*cos((3/2)*theta).^2; 
     valve.z_min_cylinder = @(theta) -0.355*ones(size(theta)) +  0.11         *0.5*(cos(3*theta)+1); 
     
     debug_plot = true; 
