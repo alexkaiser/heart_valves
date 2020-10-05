f3aaad382eb0b003987a24e459eda0059e3624b7
alexkaiser <adkaiser@gmail.com>
2020-10-05 12:15:40 -0700

    half C

--- pressure_curves/rv_pa_box_tester_pressures.m
+++ pressure_curves/rv_pa_box_tester_pressures.m
@@ -37,44 +37,44 @@ n_fourier_coeffs = 600;
 points_one_cycle_right_ventricle = [times, rv_pressure]; 
 points_one_cycle_pa              = [times, pa_pressure]; 
 
-manual_style = false; 
-if manual_style 
-    cycle_length = 0.8; 
-    base_name = 'fourier_coeffs';
-    bump_radius = .05; 
-    n_fourier_coeffs = 600; 
-
-    points_one_cycle_right_ventricle = [0.0,   0; 
-    0.02, -4; 
-    0.06, 2; 
-    0.40, 3; % 6 ; 
-    0.53, 14  / 3.8235; 
-    0.58, 120 / 3.8235; 
-    0.75, 130 / 3.8235; 
-    cycle_length, 8]; 
-
-    points_one_cycle_aorta = [0.0,  120;  
-    0.569, 80; 
-    0.58, 80; 
-    0.59, 98; 
-    0.60, 110;
-    0.61, 117;
-    0.72, 122;
-    0.73, 122;
-    % 0.745, 119; % remove notch 
-    % 0.755, 100; 
-    0.76, 122;
-    cycle_length, 120]; 
-
-    min_p_pa = 8; 
-    max_p_pa = 24; 
-    pulse_pressure_pa = max_p_pa - min_p_pa; 
-    p = points_one_cycle_aorta(:,2); 
-    pulse_pressure_aortic = max(p)-min(p); 
-    pressure_pa = (p - min(p))*(pulse_pressure_pa/pulse_pressure_aortic) + min_p_pa; 
-
-    points_one_cycle_pa = [points_one_cycle_aorta(:,1) pressure_pa]; 
-end 
+% manual_style = false; 
+% if manual_style 
+%     cycle_length = 0.8; 
+%     base_name = 'fourier_coeffs';
+%     bump_radius = .05; 
+%     n_fourier_coeffs = 600; 
+% 
+%     points_one_cycle_right_ventricle = [0.0,   0; 
+%     0.02, -4; 
+%     0.06, 2; 
+%     0.40, 3; % 6 ; 
+%     0.53, 14  / 3.8235; 
+%     0.58, 120 / 3.8235; 
+%     0.75, 130 / 3.8235; 
+%     cycle_length, 8]; 
+% 
+%     points_one_cycle_aorta = [0.0,  120;  
+%     0.569, 80; 
+%     0.58, 80; 
+%     0.59, 98; 
+%     0.60, 110;
+%     0.61, 117;
+%     0.72, 122;
+%     0.73, 122;
+%     % 0.745, 119; % remove notch 
+%     % 0.755, 100; 
+%     0.76, 122;
+%     cycle_length, 120]; 
+% 
+%     min_p_pa = 8; 
+%     max_p_pa = 24; 
+%     pulse_pressure_pa = max_p_pa - min_p_pa; 
+%     p = points_one_cycle_aorta(:,2); 
+%     pulse_pressure_aortic = max(p)-min(p); 
+%     pressure_pa = (p - min(p))*(pulse_pressure_pa/pulse_pressure_aortic) + min_p_pa; 
+% 
+%     points_one_cycle_pa = [points_one_cycle_aorta(:,1) pressure_pa]; 
+% end 
 
 
 
@@ -187,7 +187,7 @@ if abs(R_distal + R_proximal - R_total) > tol
     error('resistors not adding up correctly')
 end
 
-C_prefactor = 8.0; 
+C_prefactor = 0.5; 
 
 % timescale for pressure decrease during aortic valve closure 
 C = C_prefactor * -decay_time / (R_distal * log(P_min/P_max)); 
