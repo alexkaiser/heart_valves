264dd9972123b5edf935325d4fa0f06907ce8b70
alexkaiser <adkaiser@gmail.com>
2020-10-19 23:37:23 -0700

    full damping up to pt2 s

--- FeedbackForcer.cpp
+++ FeedbackForcer.cpp
@@ -31,7 +31,7 @@
 // #define FLOW_AVERAGER
 
 #define FULL_FLOW_CLAMP 
-#define FULL_FLOW_CLAMP_TIME 0.1
+#define FULL_FLOW_CLAMP_TIME 0.2
 
 /////////////////////////////// NAMESPACE ////////////////////////////////////
 
