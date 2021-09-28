df9945beca7d9237287c062697c6827763a7d37f
alexkaiser <adkaiser@gmail.com>
2021-09-28 12:16:44 -0700

    shorter radial

--- valve_generator/initialize_valve_data_structures_pulm_mri_box.m
+++ valve_generator/initialize_valve_data_structures_pulm_mri_box.m
@@ -213,9 +213,9 @@ tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
 tension_coeffs.c_circ_dec       = 5.65;  % circumferential 
-tension_coeffs.c_rad_dec        = 3.0;  % radial
+tension_coeffs.c_rad_dec        = 2.1;  % radial
 
-tension_coeffs.c_circ_dec_annulus = 1.9;        
+tension_coeffs.c_circ_dec_annulus = 2.25;        
 
 % tension_coeffs.c_circ_dec_free_edge = 5.0;
 tension_coeffs.c_circ_dec_free_edge_percentage = 0.0;
