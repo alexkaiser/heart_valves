5dd0317a83b84da92c51938214e73281223f3c5a
alexkaiser <adkaiser@gmail.com>
2021-09-28 22:40:30 -0700

    one layer as experiment

--- valve_generator/initialize_valve_data_structures_pulm_mri_box.m
+++ valve_generator/initialize_valve_data_structures_pulm_mri_box.m
@@ -107,11 +107,11 @@ valve.X_config_is_reference = true;
 % places this many exact copies of the leaflet downward in z 
 % spring constants are all reduced by num_copies 
 % spacing is always half a mesh width 
-valve.num_copies = 2; 
+valve.num_copies = 1; 
 
 % valve.copy_spring_weights = [1/2 1/4 1/4];
 
-valve.normal_thicken = true; 
+valve.normal_thicken = false; 
 % nominal aortic valve thickness
 valve.normal_thickness = 0.044; 
 
