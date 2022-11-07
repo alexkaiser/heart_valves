c4cf30c2f92280d35356b5b13b17c50aec5181bd
alexkaiser <adkaiser@gmail.com>
2022-11-07 15:26:26 -0800

    const fwd pressure sim

--- main_rv_pa.cpp
+++ main_rv_pa.cpp
@@ -517,9 +517,9 @@ int main(int argc, char* argv[])
             pout << "P_initial_pa = " << P_initial_pa << "\n"; 
 
             bool rcr_bcs_on = false;
-            bool resistance_bcs_on = true; 
+            bool resistance_bcs_on = false; 
             bool inductance_bcs_on = false; 
-            bool variable_resistor_on = true; 
+            bool variable_resistor_on = false; 
 
             CirculationModel_RV_PA *circ_model_rv_pa = new CirculationModel_RV_PA(input_db,
                                                                                   fourier_series_rv, 
