c6ee3e4dc584a440454754d02406214c2f44df33
alexkaiser <adkaiser@gmail.com>
2022-12-20 19:52:22 -0800

    lagrangian in parallel

--- scripts/run_temporal_averaging.py
+++ scripts/run_temporal_averaging.py
@@ -97,7 +97,7 @@ def average_eulerian_mesh_one_step(idx_mri_read, eulerian_var_names, times, cycl
                     
                 n_to_average += 1.0
 
-            # print("t = ", t, "t_reduced = ", t_reduced, "idx_mri_read = ", idx_mri_read)
+                print("t = ", t, "t_reduced = ", t_reduced, "idx_mri_read = ", idx_mri_read)
 
     for var_name in eulerian_var_names:
         # if none to average, mesh output as all zeros  
@@ -105,7 +105,50 @@ def average_eulerian_mesh_one_step(idx_mri_read, eulerian_var_names, times, cycl
             mesh[var_name] /= float(n_to_average)
 
     fname = base_name_out + str(idx_mri_read).zfill(4) + '.' + extension
+    print("trying to write base_dir/fname ", base_dir + "/" + fname)
     mesh.save(base_dir + "/" + fname)    
+
+
+def average_lagrangian_mesh_one_step(idx_mri_read, fname, times, cycle_duration, cycles_to_output, base_name_lag, dt_mri_read):
+
+        # read the base mesh 
+        meshes_mri_read = pyvista.read(fname)
+        n_to_average = 0 
+        meshes_mri_read.points *= 0.0
+
+        # average over times 
+        for idx, t in enumerate(times): 
+
+            # check if time in range 
+            cycle_num = math.floor(t / cycle_duration)
+
+            if cycle_num in cycles_to_output:
+
+                fname    = base_name_lag + str(idx).zfill(4) + '.vtu'
+
+                # time since start of this cycle 
+                t_reduced = t % cycle_duration
+
+                idx_mri_read_temp = math.floor(t_reduced / dt_mri_read)
+
+                if idx_mri_read == idx_mri_read_temp:
+                    mesh_tmp = pyvista.read(fname)
+                    meshes_mri_read.points += mesh_tmp.points
+                    n_to_average += 1.0
+
+                # print("t = ", t, "t_reduced = ", t_reduced, "idx_mri_read = ", idx_mri_read)
+
+        print("n_to_average = ", n_to_average)
+
+        # convert sums to averages
+        if n_to_average != 0:
+            # if none to average, mesh output as all zeros  
+            meshes_mri_read.points /= float(n_to_average)
+
+        outname = base_name_lag + suffix + str(idx_mri_read).zfill(4) + '.vtu'
+        print("Trying to write outname = ", outname)
+        meshes_mri_read.save(base_dir + "/" + outname)
+
 
 
 
@@ -127,12 +170,15 @@ if __name__ == '__main__':
         times.append(float(line)) 
 
     eulerian = True
-    lagrangian = True 
+    lagrangian = True
 
-    first_cycle = True
+    first_cycle = False
     second_cycle = False
     third_cycle = False
     second_third_cycle = False
+    two_three_four = True
+
+    linear_interpolation = False 
 
     if first_cycle:
         cycles_to_output = [0] # zero indexed 
@@ -150,6 +196,10 @@ if __name__ == '__main__':
         cycles_to_output = [1,2] # zero indexed 
         # set up some directories 
         base_dir = "vis_data_averaged_cycle_2_3"
+    elif two_three_four:
+        cycles_to_output = [1,2,3] # zero indexed 
+        # set up some directories 
+        base_dir = "vis_data_averaged_cycle_2_3_4"        
     else:
         cycles_to_output = [1,2,3] # zero indexed 
         # set up some directories 
@@ -159,27 +209,21 @@ if __name__ == '__main__':
     cycle_duration = 8.3250000000000002e-01
     mri_read_times_per_cycle = 10 
     dt_mri_read = cycle_duration / mri_read_times_per_cycle
-    output_times_per_cycle   = 20 
-    dt_output = cycle_duration / output_times_per_cycle
-
-
 
     if not os.path.exists(base_dir):
         os.mkdir(base_dir)
 
     if eulerian:
 
-        eulerian_var_names = ['P','Omega', 'U']
+        eulerian_var_names = ['P','U']
         # eulerian_var_names = ['U']
 
         # output file extension 
         extension = 'vtu'
 
-        suffix = "_averaged"
+        suffix  = "_averaged"
 
-        base_name_out = "eulerian_vars_mri_freq"
-
-
+        base_name_out = "eulerian_vars"
 
         # average all the Eulerian files here 
         # for idx_mri_read in range(mri_read_times_per_cycle):
@@ -189,7 +233,7 @@ if __name__ == '__main__':
         if use_multiprocessing: 
             jobs = []
             for idx_mri_read in range(mri_read_times_per_cycle):
-                p = multiprocessing.Process(target=average_eulerian_mesh_one_step, args=(idx_mri_read, eulerian_var_names, times, cycle_duration, cycles_to_output, dt_mri_read, base_dir, base_name_out, extension))
+                p = multiprocessing.Process(target=average_eulerian_mesh_one_step, args=(idx_mri_read, eulerian_var_names, times, cycle_duration, cycles_to_output, dt_mri_read, base_dir, base_name_out + suffix, extension))
                 jobs.append(p)
                 p.start()
 
@@ -197,96 +241,12 @@ if __name__ == '__main__':
                 p.join()
 
         else: 
-            for idx_mri_read in range(mri_read_times_per_cycle):
-                average_eulerian_mesh_one_step(idx_mri_read, eulerian_var_names, times, cycle_duration, cycles_to_output, dt_mri_read, base_dir, base_name_out, extension)
-
-
-        # for idx_output in range(output_times_per_cycle):
-        #     eulerian_dir_name = base_dir + '/' + 'eulerian_vars' + suffix + str(idx_output).zfill(4)
-        #     if not os.path.exists(eulerian_dir_name):
-        #         os.mkdir(eulerian_dir_name)
-
-        # only average cycle 2 
-        # cycles_to_include = [2] 
-
-        # loops over parallel data structure as outer loop 
-        # for proc_num in range(nprocs_sim):
-
-        # read and zero meshes to use to accumulate from first mesh 
-        dir_name = "eulerian_vars" + str(0).zfill(4)
-
-
-        # read all time zero meshes 
-        # meshes_mri_read = []
-        # n_to_average = []
-        # for idx_mri_read in range(mri_read_times_per_cycle):
-        #     meshes_mri_read.append(read_distributed_vtr(dir_name))
-        #     n_to_average.append(0)
-        #     for var_name in eulerian_var_names: 
-        #         meshes_mri_read[idx_mri_read][var_name] *= 0.0
-
-        meshes_mri_read = []
-        for idx_mri_read in range(mri_read_times_per_cycle):
-            fname = base_name_out + str(idx_mri_read).zfill(4) + '.' + extension
-            meshes_mri_read.append( pyvista.read(base_dir + "/" + fname) )
-
-        meshes_output = []
-        for idx_output in range(output_times_per_cycle):
-            meshes_output.append(read_distributed_vtr(dir_name))
-            for var_name in eulerian_var_names: 
-                meshes_output[idx_output][var_name] *= 0.0
-
-        # # average over times 
-        # for idx, t in enumerate(times): 
-
-        #     # check if time in range 
-        #     cycle_num = math.floor(t / cycle_duration)
-
-        #     # skip cycle one 
-        #     if cycle_num in cycles_to_output:
-        #         print("processing step ", idx)
-
-        #         dir_name = "eulerian_vars" + str(idx).zfill(4)
-
-        #         # time since start of this cycle 
-        #         t_reduced = t % cycle_duration
-
-        #         idx_mri_read = math.floor(t_reduced / dt_mri_read)
-
-        #         mesh_tmp = read_distributed_vtr(dir_name)
-
-        #         for var_name in eulerian_var_names: 
-        #             meshes_mri_read[idx_mri_read][var_name] += mesh_tmp[var_name]
-                    
-        #         n_to_average[idx_mri_read] += 1.0
-
-        #         # print("t = ", t, "t_reduced = ", t_reduced, "idx_mri_read = ", idx_mri_read)
-
-        # print("n_to_average = ", n_to_average)
-
-        # # convert sums to averages
-        # for idx_mri_read in range(mri_read_times_per_cycle):
-        #     for var_name in eulerian_var_names: 
-        #         meshes_mri_read[idx_mri_read][var_name] /= float(n_to_average[idx_mri_read])
-
-        # linearly interpolate before output         
-        for idx_mri_read in range(mri_read_times_per_cycle):
-            for var_name in eulerian_var_names: 
-                meshes_output[2*idx_mri_read][var_name] = meshes_mri_read[idx_mri_read][var_name]
-
-        for idx_mri_read in range(mri_read_times_per_cycle):
-            idx_mri_read_next = (idx_mri_read + 1) % mri_read_times_per_cycle
-            for var_name in eulerian_var_names: 
-                meshes_output[2*idx_mri_read + 1][var_name] = 0.5 * (meshes_mri_read[idx_mri_read][var_name] + meshes_mri_read[idx_mri_read_next][var_name])
-
-        for idx_output in range(output_times_per_cycle):
-            eulerian_dir_name = base_dir 
-            fname = "eulerian_vars" + suffix + str(idx_output).zfill(4) + '.' + extension
-            meshes_output[idx_output].save(eulerian_dir_name + "/" + fname)
+            for idx_mri_read in range(3): #range(mri_read_times_per_cycle):
+                average_eulerian_mesh_one_step(idx_mri_read, eulerian_var_names, times, cycle_duration, cycles_to_output, dt_mri_read, base_dir, base_name_out + suffix, extension)
 
         # summary file         
         nprocs_output = 1
-        write_pvd("eulerian_vars" + suffix, dt_output, output_times_per_cycle, extension, nprocs_output)
+        write_pvd("eulerian_vars" + suffix, dt_mri_read, mri_read_times_per_cycle, extension, nprocs_output)
         os.rename("eulerian_vars" + suffix + '.pvd', base_dir + "/eulerian_vars" + suffix + '.pvd')
 
 
@@ -307,64 +267,66 @@ if __name__ == '__main__':
                     print("vtu file not found, cannot process this file, continuing")
                     continue 
 
-                meshes_mri_read = []
-                n_to_average = []
-                for idx_mri_read in range(mri_read_times_per_cycle):
-                    meshes_mri_read.append(pyvista.read(fname))
-                    n_to_average.append(0)
-                    meshes_mri_read[idx_mri_read].points *= 0.0
+                use_multiprocessing = True
+                if use_multiprocessing: 
+                    jobs = []
+                    for idx_mri_read in range(mri_read_times_per_cycle):
+                        p = multiprocessing.Process(target=average_lagrangian_mesh_one_step, args=(idx_mri_read, fname, times, cycle_duration, cycles_to_output, base_name_lag, dt_mri_read))
+                        jobs.append(p)
+                        p.start()
 
-                meshes_output = []
-                for idx_output in range(output_times_per_cycle):
-                    meshes_output.append(pyvista.read(fname))
-                    meshes_output[idx_output].points *= 0.0
+                    for p in jobs:
+                        p.join()
 
-                # average over times 
-                for idx, t in enumerate(times): 
+                else: 
+                    for idx_mri_read in range(mri_read_times_per_cycle):
+                        average_lagrangian_mesh_one_step(idx_mri_read, fname, times, cycle_duration, cycles_to_output, base_name_lag, dt_mri_read)
 
-                    # check if time in range 
-                    cycle_num = math.floor(t / cycle_duration)
 
-                    # skip cycle one 
-                    if cycle_num in cycles_to_output:
+                # meshes_mri_read = []
+                # n_to_average = []
+                # for idx_mri_read in range(mri_read_times_per_cycle):
+                #     meshes_mri_read.append(pyvista.read(fname))
+                #     n_to_average.append(0)
+                #     meshes_mri_read[idx_mri_read].points *= 0.0
 
-                        fname    = base_name_lag + str(idx).zfill(4) + '.vtu'
+                # # average over times 
+                # for idx, t in enumerate(times): 
 
-                        # time since start of this cycle 
-                        t_reduced = t % cycle_duration
+                #     # check if time in range 
+                #     cycle_num = math.floor(t / cycle_duration)
 
-                        idx_mri_read = math.floor(t_reduced / dt_mri_read)
+                #     if cycle_num in cycles_to_output:
 
-                        mesh_tmp = pyvista.read(fname)
+                #         fname    = base_name_lag + str(idx).zfill(4) + '.vtu'
 
-                        meshes_mri_read[idx_mri_read].points += mesh_tmp.points
+                #         # time since start of this cycle 
+                #         t_reduced = t % cycle_duration
+
+                #         idx_mri_read = math.floor(t_reduced / dt_mri_read)
+
+                #         mesh_tmp = pyvista.read(fname)
+
+                #         meshes_mri_read[idx_mri_read].points += mesh_tmp.points
                             
-                        n_to_average[idx_mri_read] += 1.0
+                #         n_to_average[idx_mri_read] += 1.0
 
-                        # print("t = ", t, "t_reduced = ", t_reduced, "idx_mri_read = ", idx_mri_read)
+                #         # print("t = ", t, "t_reduced = ", t_reduced, "idx_mri_read = ", idx_mri_read)
 
-                print("n_to_average = ", n_to_average)
+                # print("n_to_average = ", n_to_average)
 
-                # convert sums to averages
-                for idx_mri_read in range(mri_read_times_per_cycle):
-                    meshes_mri_read[idx_mri_read].points /= float(n_to_average[idx_mri_read])
+                # # convert sums to averages
+                # for idx_mri_read in range(mri_read_times_per_cycle):
+                #     meshes_mri_read[idx_mri_read].points /= float(n_to_average[idx_mri_read])
 
-                # linearly interpolate before output
-                for idx_mri_read in range(mri_read_times_per_cycle):
-                    meshes_output[2*idx_mri_read].points = meshes_mri_read[idx_mri_read].points
-
-                for idx_mri_read in range(mri_read_times_per_cycle):
-                    idx_mri_read_next = (idx_mri_read + 1) % mri_read_times_per_cycle
-                    meshes_output[2*idx_mri_read + 1].points = 0.5 * (meshes_mri_read[idx_mri_read].points + meshes_mri_read[idx_mri_read_next].points)
-
-                for idx_output in range(output_times_per_cycle):
-                    fname = base_name_lag + suffix + str(idx_output).zfill(4) + '.vtu'
-                    meshes_output[idx_output].save(base_dir + "/" + fname)
-                    # os.rename(fname, base_dir + "/" + base_name_lag + suffix + '.pvd')
+                # for idx_mri_read in range(mri_read_times_per_cycle):
+                #     fname = base_name_lag + suffix + str(idx_mri_read).zfill(4) + '.vtu'
+                #     meshes_mri_read[idx_mri_read].save(base_dir + "/" + fname)
+                #     # os.rename(fname, base_dir + "/" + base_name_lag + suffix + '.pvd')
 
                 # summary file 
                 extension = 'vtu'
-                write_pvd(base_name_lag + suffix, dt_output, output_times_per_cycle, extension, 1)
+                write_pvd(base_name_lag + suffix, dt_mri_read, mri_read_times_per_cycle, extension, 1)
                 os.rename(base_name_lag + suffix + '.pvd', base_dir + "/" + base_name_lag + suffix + '.pvd')
 
 
