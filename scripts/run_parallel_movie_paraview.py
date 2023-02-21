48892116243d0560baf163fa82facfbf5de7050b
alexkaiser <adkaiser@gmail.com>
2023-02-21 15:06:45 -0800

    updated for python 3

--- scripts/run_parallel_movie_paraview.py
+++ scripts/run_parallel_movie_paraview.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 
 import os, sys, subprocess, time
 
@@ -9,27 +10,27 @@ def run_command_parallel(call_str_base, n_procs):
     for i in range(n_procs):
         call_str = call_str_base + ' ' + str(n_procs) + ' ' + str(i)
 
-        print "call str with n_procs = ", n_procs, " proc_num = ", i, "call_str = ", call_str
+        print ("call str with n_procs = ", n_procs, " proc_num = ", i, "call_str = ", call_str)
 
         p = subprocess.Popen(call_str, shell=True)
-        print "started process, p = ", p
+        print ("started process, p = ", p)
 
         processes.append(p)
 
-    print "out of call loop in orig python script "
+    print ("out of call loop in orig python script ")
     time.sleep(30)
 
     while True: 
 
-        print "new check"
+        print ("new check")
 
         all_done = True 
         for p in processes:
             if p.poll() is None:
-                print "process p = ", p, "returned None, still running"
+                print ("process p = ", p, "returned None, still running")
                 all_done = False
             else:
-                print "process p = ", p, "returned not None, is complete"
+                print ("process p = ", p, "returned not None, is complete")
 
         if all_done:
             break
@@ -40,9 +41,9 @@ def run_command_parallel(call_str_base, n_procs):
 
 if __name__ == '__main__':
 
-    print "call arguments: "
+    print ("call arguments: ")
     for arg in sys.argv:
-        print arg, " "
+        print (arg, " ")
 
     script_dir = "~/copies_scripts/"
     # script_dir = "~/mitral_fully_discrete/scripts/"
@@ -73,12 +74,12 @@ if __name__ == '__main__':
 
     if os.path.isfile('done.txt'):
         
-        print 'done.txt found'
+        print ('done.txt found')
     
         for f in os.listdir('.'):
             if f.startswith('viz'): 
                 
-                print 'Found viz directory'
+                print ('Found viz directory')
                 
                 os.chdir(f)
                 
@@ -86,31 +87,35 @@ if __name__ == '__main__':
             
 
                 call_str_base = "pvbatch " + session_file_name + " " + base_name + " "
-                print "call_str_base = ", call_str_base 
+                print ("call_str_base = ", call_str_base )
                 run_command_parallel(call_str_base, n_procs)
 
 
-    # ffmpeg from here so variables are all in place
-    movie_string = 'ffmpeg -y -framerate 60 -i '
-    movie_string += base_name
-    movie_string += '%4d.jpeg -vf scale=1920:-2 -r 60 -c:v libx264 -preset veryslow -crf 18 '
-    movie_string += base_name + '.mp4'
+                # ffmpeg from here so variables are all in place
+                movie_string = 'ffmpeg -y -framerate 60 -i '
+                movie_string += base_name
+                movie_string += '%4d.jpeg -vf scale=1920:-2 -r 60 -c:v libx264 -preset veryslow -crf 18 '
+                movie_string += base_name + '.mp4'
 
-    code = subprocess.call(movie_string, shell=True)
-    if code is None:
-        print 'something wrong in movie make, call returned prematurely'
+                code = subprocess.call(movie_string, shell=True)
+                if code is None:
+                    print ('something wrong in movie make, call returned prematurely')
 
 
-    # # reduce by 10x
-    # # 60 input, 60 output is 10x slow motion
-    # # 600 input, 60 output is real time 
+                # # reduce by 10x
+                # # 60 input, 60 output is 10x slow motion
+                # # 600 input, 60 output is real time 
 
-    movie_string = 'ffmpeg -y -framerate 600 -i '
-    movie_string += base_name
-    movie_string += '%4d.jpeg -vf scale=1920:-2 -r 60 -c:v libx264 -preset veryslow -crf 18 '
-    movie_string += base_name + '_real_time.mp4'
+                movie_string = 'ffmpeg -y -framerate 600 -i '
+                movie_string += base_name
+                movie_string += '%4d.jpeg -vf scale=1920:-2 -r 60 -c:v libx264 -preset veryslow -crf 18 '
+                movie_string += base_name + '_real_time.mp4'
 
-    code = subprocess.call(movie_string, shell=True)
-    if code is None:
-        print 'something wrong in movie make, call returned prematurely'
+                code = subprocess.call(movie_string, shell=True)
+                if code is None:
+                    print ('something wrong in movie make, call returned prematurely')
+
+    else:
+        print("could not find done.txt")
+        print("os.listdir('.') = ", os.listdir('.'))
 
