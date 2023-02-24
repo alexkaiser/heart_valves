3ab78949a8afbec3fddbd596ba754740b6a745a6
alexkaiser <adkaiser@gmail.com>
2023-02-24 12:27:42 -0800

    added 1mm of constriction to flow extender

---
+++ scripts/morph_extender.py
@@ -0,0 +1,111 @@
+import os 
+import pyvista 
+import numpy as np 
+
+
+
+def extra_radius(x, x_min, x_max, extension_value):
+
+    if x < x_min:
+        return 0.0
+    elif x < x_max:
+        # 0*(x - x_max)/(x_min - x_max) + extension_value*(x - x_min)/(x_max - x_min)
+        # linearly interpolate from 0 to extension_value
+        return extension_value*(x - x_min)/(x_max - x_min)
+    else:
+        return extension_value
+    
+
+def morph_extender(mesh, 
+                   fname_out, 
+                   mesh_boundary, 
+                   normal_direction, 
+                   masking_width, 
+                   enforce_flat_bdry = True, 
+                   flat_bdry_tolerance = 1.0e-3):
+
+
+    if normal_direction != 0:
+        raise NotImplmentedError("normal must be x direction for now")
+
+    # max to mask over 
+    x_max = np.max(mesh.points[:,normal_direction])
+    x_min = x_max - masking_width
+    print("x_min = ", x_min, "x_max = ", x_max)
+
+    # compute the centroid of the mesh 
+    centroid = np.mean(mesh_boundary.points, axis=0)
+    print("centroid = ", centroid)
+
+    mesh_adjusted = mesh 
+
+    # 1 mm out at the ends 
+    extension_value = 1.0
+
+    for idx, pt in enumerate(mesh.points):
+
+        normal = (pt - centroid)
+        normal[0] = 0.0
+        normal /= np.linalg.norm(normal) 
+
+        extra_r = extra_radius(pt[0], x_min, x_max, extension_value)
+
+        increment = [0, extra_r * normal[1], extra_r * normal[2]]
+
+        if enforce_flat_bdry:
+            if abs(pt[0] - x_max) < flat_bdry_tolerance:
+                pt[0] = x_max
+
+        mesh_adjusted.points[idx] = pt + increment 
+
+    mesh_adjusted.save(fname_out)
+    return mesh_adjusted
+
+
+if __name__== "__main__":
+
+    fname = "16_meshed_pt25mm_3_layer_5_layer_inlet_outlet.stl"
+    fname_out = "17_meshed_pt25mm_3_layer_5_layer_inlet_outlet_constriction.stl"
+
+    mesh = pyvista.read(fname)
+
+    bdry_filename = 'lvot_bdry_384_layer_3_pt5mm.vtu'
+    bdry_filename_out = 'lvot_bdry_384_layer_3_pt5mm_constriction.vtu'
+    mesh_boundary = pyvista.read(bdry_filename)
+
+    # x direction 
+    normal_direction = 0
+
+    # mesh in mm, mask 1 cm worth 
+    masking_width = 8.0
+
+    # if true, adjusts x component to be exactly equal to this value 
+    enforce_flat_bdry = True
+    flat_bdry_tolerance = 1.0e-3
+
+    mesh_adjusted = morph_extender(mesh, 
+                                   fname_out, 
+                                   mesh_boundary, 
+                                   normal_direction, 
+                                   masking_width, 
+                                   enforce_flat_bdry, 
+                                   flat_bdry_tolerance)
+
+    # pyvista.plot(mesh_adjusted)
+
+    mesh_boundary_copy = mesh_boundary
+
+    mesh_boundary_adjusted = morph_extender(mesh_boundary_copy, 
+                                            bdry_filename_out, 
+                                            mesh_boundary, 
+                                            normal_direction, 
+                                            masking_width, 
+                                            enforce_flat_bdry, 
+                                            flat_bdry_tolerance)
+
+    # pyvista.plot(mesh_boundary_adjusted)
+
+    
+
+
+
