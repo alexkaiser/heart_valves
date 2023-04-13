70dff19f617df673ad13b1aac91cd661108e949b
alexkaiser <adkaiser@gmail.com>
2023-04-13 13:04:24 -0700

    removed unused and bad normal override option

--- valve_generator/coordinate_transformation_vertices.m
+++ valve_generator/coordinate_transformation_vertices.m
@@ -1,4 +1,4 @@
-function [vertices normal centroid R] = coordinate_transformation_vertices(vertices, ring_boundary_file, R_0, T_0, normal_override, origin_override)
+function [vertices normal centroid R] = coordinate_transformation_vertices(vertices, ring_boundary_file, R_0, T_0)
 % 
 % takes file containing ring points 
 % applies rigid translations and rotations that takes the unit circle to 
@@ -8,44 +8,36 @@ tol = 1e-14;
 
 [~, n_vertices] = size(vertices); 
 
-if exist('normal_override', 'var') && exist('origin_override', 'var')
-    if ~isempty(ring_boundary_file)
-        warning('overriding normal and origin, ignoring boundary file'); 
-    end 
-    
-    normal = normal_override; 
-    centroid = origin_override; 
-    
-else 
-    f = fopen(ring_boundary_file, 'r'); 
-    vertices_ring_bdry = fscanf(f, '%f'); 
-    fclose(f); 
 
-    % first is number of vertices 
-    n_pts_ring_from_file = vertices_ring_bdry(1); 
+f = fopen(ring_boundary_file, 'r'); 
+vertices_ring_bdry = fscanf(f, '%f'); 
+fclose(f); 
 
-    % crop to keep just this 
-    vertices_ring_bdry = vertices_ring_bdry(2:end); 
+% first is number of vertices 
+n_pts_ring_from_file = vertices_ring_bdry(1); 
 
-    vertices_ring_bdry = reshape(vertices_ring_bdry, 3, []); 
-    [~, n_pts_ring] = size(vertices_ring_bdry); 
+% crop to keep just this 
+vertices_ring_bdry = vertices_ring_bdry(2:end); 
 
-    if n_pts_ring ~= n_pts_ring_from_file
-        error('inconsistent number of points from header and array size'); 
-    end 
+vertices_ring_bdry = reshape(vertices_ring_bdry, 3, []); 
+[~, n_pts_ring] = size(vertices_ring_bdry); 
 
-    centroid = zeros(3,1); 
-    for i=1:3
-        centroid(i) = mean(vertices_ring_bdry(i,:)); 
-    end 
-
-    normal = cross(vertices_ring_bdry(:,1) - centroid, vertices_ring_bdry(:, floor(n_pts_ring/4)) - centroid); 
-    % take the z direction up normal 
-    if normal(3) < 0
-        normal = -normal; 
-    end 
-    normal = normal / norm(normal); 
+if n_pts_ring ~= n_pts_ring_from_file
+    error('inconsistent number of points from header and array size'); 
 end 
+
+centroid = zeros(3,1); 
+for i=1:3
+    centroid(i) = mean(vertices_ring_bdry(i,:)); 
+end 
+
+normal = cross(vertices_ring_bdry(:,1) - centroid, vertices_ring_bdry(:, floor(n_pts_ring/4)) - centroid); 
+% take the z direction up normal 
+if normal(3) < 0
+    normal = -normal; 
+end 
+normal = normal / norm(normal); 
+
 
 % initial rotation if requested 
 if exist('T_0', 'var')
