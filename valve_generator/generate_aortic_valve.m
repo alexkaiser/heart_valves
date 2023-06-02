63b7a5bcf5d68fb9f9aa28fab46d51bed3677fd5
alexkaiser <adkaiser@gmail.com>
2023-06-01 23:31:24 -0700

    double layer springs

--- valve_generator/generate_aortic_valve.m
+++ valve_generator/generate_aortic_valve.m
@@ -47,7 +47,7 @@ for N_each = N_each_range
     % Many parameters are in this script 
 
     pulm = false; 
-    true_bicuspid = true; 
+    true_bicuspid = false; 
     
     if pulm
         valve = initialize_valve_data_structures_pulm_mri_box(N); 
