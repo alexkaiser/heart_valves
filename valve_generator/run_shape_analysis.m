f37a287e66f61d511670ac701a0d1bcc0577ca39
alexkaiser <adkaiser@gmail.com>
2023-07-14 12:12:03 -0700

    working on new cases

--- valve_generator/run_shape_analysis.m
+++ valve_generator/run_shape_analysis.m
@@ -28,7 +28,7 @@ if basic_test
 end 
 
 
-two_frame_summary = false; 
+two_frame_summary = true; 
 
 if two_frame_summary
     
@@ -44,31 +44,31 @@ if two_frame_summary
 %         "aortic_15051277_384_98d4f6c_true_bicuspid_circ_3pt9_1pt57d_rad_1pt4", ...
 %         "aortic_16698035_384_23a9213_true_bicuspid_circ_3pt9_1pt6d_rad_1pt65_less_bowl"]; 
    
-    names_struct(1).dir =  "aortic_14234920_384_a75f53c_true_bicuspid_circ_2pt8_1pt1d_rad_1pt4_new_initial_cond"; 
+    names_struct(1).dir =  "aortic_22425104_384_40405e1_true_bicuspid_circ_2pt7_1pt1d_rad_1pt6_semifinal_1"; 
     names_struct(1).circ =  "2.8"; 
     names_struct(1).circ_over_d =  "1.1d"; 
     names_struct(1).rad =  "1.4"; 
     names_struct(1).comment = ""; 
 
-    names_struct(2).dir =  "aortic_14103984_384_28eb02c_true_bicuspid_circ_3pt0_1pt2d_rad_1pt7_new_initial_cond"; 
+    names_struct(2).dir =  "aortic_22389012_384_38d07aa_true_bicuspid_circ_3pt0_1pt2d_rad_1pt7_semifinal_1"; 
     names_struct(2).circ =  "3.0"; 
     names_struct(2).circ_over_d =  "1.2d"; 
     names_struct(2).rad =  "1.7"; 
     names_struct(2).comment = "";
     
-    names_struct(3).dir =  "aortic_14097127_384_4b0882c_true_bicuspid_circ_3pt5_1pt4d_rad_1pt7_new_initial_cond_2"; 
+    names_struct(3).dir =  "aortic_22388339_384_c68c6b7__true_bicuspid_circ_3pt5_1pt4d_rad_1pt7_semifinal_1"; 
     names_struct(3).circ =  "3.5"; 
     names_struct(3).circ_over_d =  "1.4d"; 
     names_struct(3).rad =  "1.7"; 
     names_struct(3).comment = "";
     
-    names_struct(4).dir =  "aortic_14095958_384_67bdcde_true_bicuspid_circ_3pt9_1pt65d_rad_1pt7_new_initial_cond_2"; 
+    names_struct(4).dir =  "aortic_22387241_384_8e666fd_true_bicuspid_circ_3pt9_1pt65d_rad_1pt7_semifinal_1"; 
     names_struct(4).circ =  "3.9"; 
-    names_struct(4).circ_over_d =  "1.57d"; 
+    names_struct(4).circ_over_d =  "1.6d"; 
     names_struct(4).rad =  "1.7"; 
     names_struct(4).comment = "At circ";
     
-    names_struct(5).dir =  "aortic_14149904_384_a75f53c_true_bicuspid_circ_4pt5_1pt8d_rad_1pt7_new_initial_cond"; 
+    names_struct(5).dir =  "aortic_22389594_384_8bb4d38_true_bicuspid_circ_4pt5_1pt8d_rad_1pt7_semifinal_1"; 
     names_struct(5).circ =  "4.5"; 
     names_struct(5).circ_over_d =  "1.8d"; 
     names_struct(5).rad =  "1.7"; 
@@ -80,35 +80,35 @@ if two_frame_summary
 %     names_struct(6).rad =  "1.7"; 
 %     names_struct(6).comment = "Circ prestretch 1.08";
 %     
-    names_struct(6).dir =  "aortic_15051277_384_98d4f6c_true_bicuspid_circ_3pt9_1pt57d_rad_1pt4"; 
-    names_struct(6).circ =  "3.9"; 
-    names_struct(6).circ_over_d =  "1.57d"; 
-    names_struct(6).rad =  "1.4"; 
-    names_struct(6).comment = "Less tall";
+%     names_struct(6).dir =  "aortic_15051277_384_98d4f6c_true_bicuspid_circ_3pt9_1pt57d_rad_1pt4"; 
+%     names_struct(6).circ =  "3.9"; 
+%     names_struct(6).circ_over_d =  "1.57d"; 
+%     names_struct(6).rad =  "1.4"; 
+%     names_struct(6).comment = "Less tall";
+%     
+%     names_struct(7).dir =  "aortic_16698035_384_23a9213_true_bicuspid_circ_3pt9_1pt6d_rad_1pt65_less_bowl"; 
+%     names_struct(7).circ =  "3.9"; 
+%     names_struct(7).circ_over_d =  "1.6d"; 
+%     names_struct(7).rad =  "1.65"; 
+%     names_struct(7).comment = "Less bowl";
+%     
+%     names_struct(8).dir =  "aortic_21192946_384_ae92093_true_bicuspid_circ_3pt75_1pt5d_rad_1pt4_less_bowl_layers_2e4"; 
+%     names_struct(8).circ =  "3.75"; 
+%     names_struct(8).circ_over_d =  "1.5d"; 
+%     names_struct(8).rad =  "1.4"; 
+%     names_struct(8).comment = "Less bowl \& tall";
+%     
+%     names_struct(9).dir =  "aortic_21254567_384_fc9c4b3_true_bicuspid_circ_3pt0_1pt2d_rad_1pt4_less_bowl_layers_2e4"; 
+%     names_struct(9).circ =  "3.0"; 
+%     names_struct(9).circ_over_d =  "1.2d"; 
+%     names_struct(9).rad =  "1.4"; 
+%     names_struct(9).comment = "Less bowl \& tall";
     
-    names_struct(7).dir =  "aortic_16698035_384_23a9213_true_bicuspid_circ_3pt9_1pt6d_rad_1pt65_less_bowl"; 
-    names_struct(7).circ =  "3.9"; 
-    names_struct(7).circ_over_d =  "1.6d"; 
-    names_struct(7).rad =  "1.65"; 
-    names_struct(7).comment = "Less bowl";
-    
-    names_struct(8).dir =  "aortic_21192946_384_ae92093_true_bicuspid_circ_3pt75_1pt5d_rad_1pt4_less_bowl_layers_2e4"; 
-    names_struct(8).circ =  "3.75"; 
-    names_struct(8).circ_over_d =  "1.5d"; 
-    names_struct(8).rad =  "1.4"; 
-    names_struct(8).comment = "Less bowl \& tall";
-    
-    names_struct(9).dir =  "aortic_21254567_384_fc9c4b3_true_bicuspid_circ_3pt0_1pt2d_rad_1pt4_less_bowl_layers_2e4"; 
-    names_struct(9).circ =  "3.0"; 
-    names_struct(9).circ_over_d =  "1.2d"; 
-    names_struct(9).rad =  "1.4"; 
-    names_struct(9).comment = "Less bowl \& tall";
-    
-    data_idx_to_output = [4,6,7,8]; 
+    % data_idx_to_output = [4,6,7,8]; 
+    data_idx_to_output = 1:5; 
     
     n_times = 2; 
-    idx_frames = [242, 407, 724, 889]; 
-    % idx_frames = [242, 407]; 
+    idx_frames = [775, 893]; 
     
     run_layers = false;
     if run_layers
@@ -149,10 +149,10 @@ if two_frame_summary
                 indices_global = params.layer_indices(layer).indices_global; 
                 
                 % proceed if this timestep is not available 
-                if idx_frames(i) > length(times)
-                    cd .. 
-                    continue; 
-                end 
+%                 if idx_frames(i) > length(times)
+%                     cd .. 
+%                     continue; 
+%                 end 
                 
                 % times_tmp = times(idx_frames)'; 
 
@@ -178,7 +178,7 @@ if two_frame_summary
     %             free_edge_length_total = zeros(n_times, 1); 
 
                 % time files are zero indexed 
-                file_name = sprintf('aortic_no_partition_384%04d.csv', idx_frames(i)-1); 
+                file_name = sprintf('aortic_no_partition_384%04d.csv', idx_frames(i)); 
 
                 valve_with_reference.leaflets(1) = import_valve_from_csv(valve_with_reference, file_name, indices_global, data_dir, run_inv_transform); 
 
@@ -248,7 +248,8 @@ if two_frame_summary
                 Eh_from_min_over_Gh = Eh_from_minimum ./ Gh;
 
 
-                fprintf('%s & %s & %s & %s & ', names_struct(data_idx).circ_over_d, names_struct(data_idx).circ, names_struct(data_idx).rad, names_struct(data_idx).comment); 
+                % fprintf('%s & %s & %s & ', names_struct(data_idx).circ_over_d, names_struct(data_idx).circ, names_struct(data_idx).rad); 
+                fprintf('%s & %s & %s & ', names_struct(data_idx).circ_over_d); 
 
                 fprintf('%.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f ', orifice_area_all_points, Gh(1), Gh_over_radius(1), min_height_center(1), Eh(1), Eh_from_minimum(1), ...
                                                 Eh_over_Gh(1), Eh_from_min_over_Gh(1), free_edge_length(1), free_edge_length_over_diameter(1)); 
@@ -408,7 +409,7 @@ end
 
 
 
-run_stl_export = true; 
+run_stl_export = false; 
 if run_stl_export
        
     names_struct(1).dir =  "aortic_14234920_384_a75f53c_true_bicuspid_circ_2pt8_1pt1d_rad_1pt4_new_initial_cond"; 
