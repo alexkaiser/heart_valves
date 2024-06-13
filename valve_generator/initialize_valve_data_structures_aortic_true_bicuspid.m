b42c2360994b2f0581f48316d4ea42ccbc182c68
alexkaiser <adkaiser@gmail.com>
2024-06-12 19:12:51 -0700

    circ 1.57d rad 1.5 even annulus

--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -233,10 +233,10 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 2.93;  % circumferential 
+tension_coeffs.c_circ_dec       = 3.08;  % circumferential 
 tension_coeffs.c_rad_dec        = 1.5;  % radial
 
-tension_coeffs.c_circ_dec_annulus = 1.95;
+tension_coeffs.c_circ_dec_annulus = 1.99;
 
 % tension_coeffs.c_circ_dec_free_edge = 5.0;
 tension_coeffs.c_circ_dec_free_edge_percentage = 0.0;
