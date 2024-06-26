d0af0084c27314231f015d714b22a267416adf57
alexkaiser <adkaiser@gmail.com>
2024-06-25 22:17:27 -0700

    c 1pt57 r 1.4

--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -233,8 +233,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 3.08;  % circumferential 
-tension_coeffs.c_rad_dec        = 1.5;  % radial
+tension_coeffs.c_circ_dec       = 3.26;  % circumferential 
+tension_coeffs.c_rad_dec        = 1.44;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.99;
 
