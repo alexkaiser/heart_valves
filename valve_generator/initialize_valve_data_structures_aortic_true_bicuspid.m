4fe2ab1e508fc17e56218093e50543ab83157a8f
alexkaiser <adkaiser@gmail.com>
2024-07-15 18:10:16 -0700

    new initial condition, same as 10a8ea4, c 1.57 r 1.1

--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -233,10 +233,10 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 4.88;  % circumferential 
-tension_coeffs.c_rad_dec        = 0.8;  % radial
+tension_coeffs.c_circ_dec       = 3.9;  % circumferential 
+tension_coeffs.c_rad_dec        = 1.17;  % radial
 
-tension_coeffs.c_circ_dec_annulus = 2.06;
+tension_coeffs.c_circ_dec_annulus = 2.02;
 
 % tension_coeffs.c_circ_dec_free_edge = 5.0;
 tension_coeffs.c_circ_dec_free_edge_percentage = 0.0;
