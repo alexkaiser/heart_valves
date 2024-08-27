2a5b1e40798ff869197e923d2641045957f5ffe0
alexkaiser <adkaiser@gmail.com>
2024-08-26 22:43:12 -0700

    annulus curve from literature, circ 1pt57d rad 1pt4, theta  annulus spacing

--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -267,10 +267,10 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 3.1;  % circumferential 
-tension_coeffs.c_rad_dec        = 1.08;  % radial
+tension_coeffs.c_circ_dec       = 3.21;  % circumferential 
+tension_coeffs.c_rad_dec        = 1.3;  % radial
 
-tension_coeffs.c_circ_dec_annulus = 1.95;
+tension_coeffs.c_circ_dec_annulus = 1.925;
 
 % tension_coeffs.c_circ_dec_free_edge = 5.0;
 tension_coeffs.c_circ_dec_free_edge_percentage = 0.0;
