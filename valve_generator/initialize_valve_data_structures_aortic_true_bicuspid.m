96b489c9494f1f9722736e42c1c77fa14ed3ef5e
alexkaiser <adkaiser@gmail.com>
2024-09-18 16:04:48 -0700

    circ 1pt4d rad 0pt8,  new init cond power search

--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -268,8 +268,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 3.17;  % circumferential 
-tension_coeffs.c_rad_dec        = 0.95;  % radial
+tension_coeffs.c_circ_dec       = 3.565;  % circumferential 
+tension_coeffs.c_rad_dec        = 0.685;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.91;
 
