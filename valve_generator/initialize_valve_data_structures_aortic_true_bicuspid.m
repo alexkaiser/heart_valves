4de2d99e4fd1aced201c1f2b8ea56bb36d8226cb
alexkaiser <adkaiser@gmail.com>
2024-09-24 22:50:19 -0700

    1pt57d 0pt8 fixed comm height

--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -268,8 +268,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 3.545;  % circumferential 
-tension_coeffs.c_rad_dec        = 0.994;  % radial
+tension_coeffs.c_circ_dec       = 4.205;  % circumferential 
+tension_coeffs.c_rad_dec        = 0.75;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.91;
 
