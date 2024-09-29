f107e7511d77a40015be65cea4f56b2c80124a53
alexkaiser <adkaiser@gmail.com>
2024-09-28 23:43:25 -0700

    1pt57d, 1pt1, 3d spacing, annulus to top, no post, radial attachment .5*r_stj

--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -272,8 +272,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 2.95;  % circumferential 
-tension_coeffs.c_rad_dec        = 1.124;  % radial
+tension_coeffs.c_circ_dec       = 3.36;  % circumferential 
+tension_coeffs.c_rad_dec        = 0.94;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.91;
 
