3957327dc03acf1dbe6e2e38252544220c8f5ffb
alexkaiser <adkaiser@gmail.com>
2024-12-24 13:19:21 -0800

    1pt4d 0pt8 stj=vbr=25 mm

--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -272,8 +272,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 3.3;  % circumferential 
-tension_coeffs.c_rad_dec        = 0.855;  % radial
+tension_coeffs.c_circ_dec       = 3.605;  % circumferential 
+tension_coeffs.c_rad_dec        = 0.61;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.91;
 
