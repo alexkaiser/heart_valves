de1a494ea3b71c91d206ab87ff0bcee65caed8ed
alexkaiser <adkaiser@gmail.com>
2025-02-11 17:31:01 -0800

    updated shape analysis for multileaflet data structure

--- valve_generator/import_valve_from_csv.m
+++ valve_generator/import_valve_from_csv.m
@@ -1,4 +1,4 @@
-function leaflet = import_valve_from_csv(valve, file_name, indices_global, directory, run_inv_transform, leaflet_num)
+function leaflets = import_valve_from_csv(valve, params, file_name, directory, run_inv_transform)
 
 if exist('directory', 'var')
     file_with_path = fullfile(directory, file_name); 
@@ -10,31 +10,46 @@ if ~exist('run_inv_transform', 'var')
     run_inv_transform = false; 
 end 
 
-if ~exist('leaflet_num', 'var')
-    leaflet_num = 1; 
-end 
+[n_layers, n_leaflets] = size(params.layer_indices);
 
-leaflet = valve.leaflets(leaflet_num); 
+% just an allocation 
+leaflets(n_layers, n_leaflets) = valve.leaflets(1);
 
-% file_with_path
 
-vertices = csvread(file_with_path)'; 
+for leaflet_num = 1:n_leaflets
+    for layer_num = 1:n_layers 
 
-if run_inv_transform
-    vertices = coordinate_transformation_vertices(vertices, ...
-                                                  valve.transformation_vertex_file, ...
-                                                  valve.initial_rotation_aortic, ...
-                                                  valve.initial_translation_aortic, ...
-                                                  run_inv_transform); 
-end 
+        leaflet_temp = valve.leaflets(leaflet_num); 
+        
+        indices_global = params.layer_indices(layer_num, leaflet_num).indices_global;
 
-j_max  = leaflet.j_max; 
-k_max  = leaflet.k_max; 
+        % file_with_path
 
-for j = 1:j_max
-    for k = 1:k_max
-        vertex_idx = indices_global(j,k) + 1; 
-        leaflet.X(:,j,k) = vertices(:,vertex_idx);
+        vertices = csvread(file_with_path)'; 
+
+        if run_inv_transform
+            vertices = coordinate_transformation_vertices(vertices, ...
+                                                          valve.transformation_vertex_file, ...
+                                                          valve.initial_rotation_aortic, ...
+                                                          valve.initial_translation_aortic, ...
+                                                          run_inv_transform); 
+        end 
+
+        j_max  = leaflet_temp.j_max; 
+        k_max  = leaflet_temp.k_max; 
+
+        for j = 1:j_max
+            for k = 1:k_max
+                vertex_idx = indices_global(j,k) + 1; 
+                leaflet_temp.X(:,j,k) = vertices(:,vertex_idx);
+            end 
+        end 
+
+        leaflets(layer_num, leaflet_num) = leaflet_temp; 
+        
     end 
 end 
-
+    
+    
+    
+    
--- valve_generator/shape_analysis_aortic.m
+++ valve_generator/shape_analysis_aortic.m
@@ -1,16 +1,11 @@
 function [Gh, Eh, Eh_from_minimum, min_height_center, free_edge_length, ...
-          orifice_area_free_edge, orifice_area_all_points, free_edge_length_total] = shape_analysis_aortic(leaflet, leaflet_layer_2 , leaflet_layer_3, debug_plots)
+          orifice_area_free_edge, orifice_area_all_points, free_edge_length_total] = shape_analysis_aortic(leaflets, debug_plots)
 
-j_max  = leaflet.j_max; 
-k_max  = leaflet.k_max; 
-N_each = leaflet.N_each; 
+j_max  = leaflets(1,1).j_max; 
+k_max  = leaflets(1,1).k_max; 
+N_each = leaflets(1,1).N_each; 
 
-
-if isfield(leaflet, 'N_leaflets')
-    N_leaflets = leaflet.N_leaflets; 
-else 
-    N_leaflets = 3; 
-end 
+[n_layers, n_leaflets] = size(leaflets);
 
 % all on first leaflet for now, no arrays
 % Eh = zeros(N_leaflets,1); 
@@ -23,20 +18,16 @@ Gh = 0;
 min_height_center = 0;
 free_edge_length = 0;
 
+layer_idx_ventricular = 1; 
+layer_idx_aortic = n_layers; 
 
-X_layer_ventricular = leaflet.X;
-X_layer_aortic = leaflet_layer_3.X; 
 
 N_leaflets = 1; 
 
 % height 
 
-for comm_idx = 1% :N_leaflets
+for leaflet_idx = 1:N_leaflets
 
-    min_idx = (comm_idx-1)*N_each;    
-    
-    max_idx = N_each + min_idx; 
-   
     center_idx_j = N_each/2; 
 
     j = center_idx_j; 
@@ -45,22 +36,22 @@ for comm_idx = 1% :N_leaflets
     
         j_nbr_tmp = j;
         k_nbr_tmp = k-1; 
-        [valid j_nbr k_nbr j_spr k_spr] = get_indices(leaflet_layer_3, j, k, j_nbr_tmp, k_nbr_tmp); 
+        [valid j_nbr k_nbr j_spr k_spr] = get_indices(leaflets(layer_idx_aortic, leaflet_idx), j, k, j_nbr_tmp, k_nbr_tmp); 
         if ~valid 
             error('trying to compute lengths with an invalid rest length')
         end
 
-        X_temp = X_layer_aortic(:,j,k);
-        X_nbr = X_layer_aortic(:,j_nbr,k_nbr); 
+        X_temp = leaflets(layer_idx_aortic, leaflet_idx).X(:,j,k);
+        X_nbr = leaflets(layer_idx_aortic, leaflet_idx).X(:,j_nbr,k_nbr); 
 
-        Gh(comm_idx) = Gh(comm_idx) + norm(X_temp - X_nbr);        
+        Gh(leaflet_idx) = Gh(leaflet_idx) + norm(X_temp - X_nbr);        
     end 
     
-    Eh(comm_idx) = X_layer_aortic(3,j,k_max); 
+    Eh(leaflet_idx) = leaflets(layer_idx_aortic, leaflet_idx).X(3,j,k_max); 
     
     % min_height_center(comm_idx) = min(X(3,j,:));
     
-    min_height_center(comm_idx) = min(min(X_layer_ventricular(3,min_idx+1:max_idx-1,:)));
+    min_height_center(leaflet_idx) = min(min(leaflets(layer_idx_ventricular, leaflet_idx).X(3,:,:)));
     
 end
 
@@ -68,22 +59,22 @@ end
 Eh_from_minimum = Eh - min_height_center; 
 
 % free edge lengths 
-for comm_idx = 1 % :N_leaflets
+for leaflet_idx=1:N_leaflets
     
     for j=2:j_max
         k = k_max; 
 
         j_nbr_tmp = j-1; 
         k_nbr_tmp = k; 
-        [valid j_nbr k_nbr j_spr k_spr] = get_indices(leaflet, j, k, j_nbr_tmp, k_nbr_tmp); 
+        [valid j_nbr k_nbr j_spr k_spr] = get_indices(leaflets(layer_idx_ventricular, leaflet_idx), j, k, j_nbr_tmp, k_nbr_tmp); 
         if ~valid 
             error('trying to compute lengths with an invalid rest length')
         end
 
-        X_temp = X_layer_ventricular(:,j,k);
-        X_nbr = X_layer_ventricular(:,j_nbr,k_nbr); 
+        X_temp = leaflets(layer_idx_ventricular, leaflet_idx).X(:,j,k);
+        X_nbr = leaflets(layer_idx_ventricular, leaflet_idx).X(:,j_nbr,k_nbr); 
 
-        free_edge_length(comm_idx) = free_edge_length(comm_idx) + norm(X_temp - X_nbr);        
+        free_edge_length(leaflet_idx) = free_edge_length(leaflet_idx) + norm(X_temp - X_nbr);        
         
     end
     
@@ -91,7 +82,13 @@ end
 
 free_edge_length_total = sum(free_edge_length); 
 
-orifice_area_free_edge = polyarea(X_layer_ventricular(1,:,k_max), X_layer_ventricular(2,:,k_max)); 
+
+X_free_edge_points = [];
+for leaflet_idx = 1:N_leaflets
+    X_free_edge_points = [X_free_edge_points, leaflets(layer_idx_ventricular, leaflet_idx).X(:,:,k_max)];
+end 
+
+orifice_area_free_edge = polyarea(X_free_edge_points(1,:), X_free_edge_points(2,:)); 
 
 
 x_component_vector = []; 
@@ -100,62 +97,49 @@ x_component_norm_inverted_vector = [];
 y_component_norm_inverted_vector = []; 
 
 
-if exist('leaflet_layer_2','var') && exist('leaflet_layer_3','var')
-    layer_range = 1:3; 
-else 
-    layer_range = 1; 
-end 
 
+for leaflet_idx = 1:n_leaflets
+    for layer_idx = 1:n_layers 
 
-for layer_idx = layer_range 
+        X_temp = leaflets(layer_idx, leaflet_idx).X; 
+        
+        % orifice area from conv hull 
+        % multiply all vectors by one over their norm 
+        % take convex hull then reinvert 
+        % for interior area 
+        vector_norms_xy = zeros(j_max,k_max); 
 
-    if layer_idx == 1 
-        X_temp = leaflet.X; 
-    elseif layer_idx == 2 
-        X_temp = leaflet_layer_2.X;
-    elseif layer_idx == 3
-        X_temp = leaflet_layer_3.X;
-    else 
-        error('unsupported layer idx')
-    end
-    
-    
-    % orifice area from conv hull 
-    % multiply all vectors by one over their norm 
-    % take convex hull then reinvert 
-    % for interior area 
-    vector_norms_xy = zeros(j_max,k_max); 
+        x_component_norm_inverted = zeros(j_max,k_max); 
+        y_component_norm_inverted = zeros(j_max,k_max); 
 
-    x_component_norm_inverted = zeros(j_max,k_max); 
-    y_component_norm_inverted = zeros(j_max,k_max); 
-
-    for j=1:j_max
-        for k=1:k_max 
-            vector_norms_xy(j,k) = norm(X_temp(1:2,j,k)); 
-            x_component_norm_inverted(j,k) = X_temp(1,j,k) / vector_norms_xy(j,k)^2; 
-            y_component_norm_inverted(j,k) = X_temp(2,j,k) / vector_norms_xy(j,k)^2; 
+        for j=1:j_max
+            for k=1:k_max 
+                vector_norms_xy(j,k) = norm(X_temp(1:2,j,k)); 
+                x_component_norm_inverted(j,k) = X_temp(1,j,k) / vector_norms_xy(j,k)^2; 
+                y_component_norm_inverted(j,k) = X_temp(2,j,k) / vector_norms_xy(j,k)^2; 
+            end 
         end 
+
+        x_component = X_temp(1,:,:); 
+        y_component = X_temp(2,:,:); 
+        x_component_vector = [x_component_vector; x_component(:)]; 
+        y_component_vector = [y_component_vector; y_component(:)]; 
+
+        x_component_norm_inverted_vector = [x_component_norm_inverted_vector; x_component_norm_inverted(:)]; 
+        y_component_norm_inverted_vector = [y_component_norm_inverted_vector; y_component_norm_inverted(:)]; 
+
     end 
-
-    x_component = X_temp(1,:,:); 
-    y_component = X_temp(2,:,:); 
-    x_component_vector = [x_component_vector; x_component(:)]; 
-    y_component_vector = [y_component_vector; y_component(:)]; 
-
-    x_component_norm_inverted_vector = [x_component_norm_inverted_vector; x_component_norm_inverted(:)]; 
-    y_component_norm_inverted_vector = [y_component_norm_inverted_vector; y_component_norm_inverted(:)]; 
-
 end 
     
 shrink_factor = 1; 
 conv_hull_idx = boundary(x_component_norm_inverted_vector, y_component_norm_inverted_vector, shrink_factor); 
 
-% if exist('debug_plots', 'var') && debug_plots
-%     figure; 
-%     plot(x_component_norm_inverted_vector, y_component_norm_inverted_vector, 'b*')
-%     hold on           
-%     plot(x_component_norm_inverted_vector(conv_hull_idx), y_component_norm_inverted_vector(conv_hull_idx), 'r-')
-% end 
+if exist('debug_plots', 'var') && debug_plots
+    figure; 
+    plot(x_component_norm_inverted_vector, y_component_norm_inverted_vector, 'b*')
+    hold on           
+    plot(x_component_norm_inverted_vector(conv_hull_idx), y_component_norm_inverted_vector(conv_hull_idx), 'r-')
+end 
 
 x_component_interior_polygon = x_component_vector(conv_hull_idx); 
 y_component_interior_polygon = y_component_vector(conv_hull_idx); 
