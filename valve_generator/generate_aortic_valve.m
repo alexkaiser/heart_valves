f6a70ca4aeb0cdc15165f999268568b6ac2e6115
alexkaiser <adkaiser@gmail.com>
2025-10-04 14:32:30 -0700

    vbr 19 stj 17 fel/stj 1.57, rest gh pt97 gh/d pt5

--- valve_generator/generate_aortic_valve.m
+++ valve_generator/generate_aortic_valve.m
@@ -47,10 +47,10 @@ for N_each = N_each_range
     % Many parameters are in this script 
 
     pulm = false; 
-    true_bicuspid = false;
+    true_bicuspid = true;
     fish = false;
     quad = false; 
-    pent = true; 
+    pent = false; 
     
     if pulm
         valve = initialize_valve_data_structures_pulm_mri_box(N); 
--- valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
+++ valve_generator/initialize_valve_data_structures_aortic_true_bicuspid.m
@@ -218,8 +218,8 @@ valve.p_final_fixed_comm = 0.1 * MMHG_TO_CGS;
 
 valve.L = 2.25; 
 
-r_stj = 2.5/2; % 25 mm valve 
-r_temp = 2.5/2; % vbr radius
+r_stj = 1.67 / 2;
+r_temp = 1.93 / 2; % vbr radius
 hc = 0.5 * r_stj; 
 h1 = 1.4 * r_stj - hc; 
 r_commissure = r_stj; 
@@ -263,8 +263,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 3.34;  % circumferential 
-tension_coeffs.c_rad_dec        = 1.99;  % radial
+tension_coeffs.c_circ_dec       = 3.16;  % circumferential 
+tension_coeffs.c_rad_dec        = 1.18;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 1.91;
 
