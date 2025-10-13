da0fff8ffb3c27b05e02db9acd25bfda6fbadefc
alexkaiser <adkaiser@gmail.com>
2025-10-13 09:38:04 -0700

    trying new parfor in cells file

--- valve_generator/generate_cells_file.m
+++ valve_generator/generate_cells_file.m
@@ -26,7 +26,46 @@ file_list = dir('aortic_*.csv');
 
 add_mechanics = false;
 
+for i = 1:1
+    
+    file_name = file_list(i).name; 
+    
+    if ~(contains(file_name, '_vertices.csv') || contains(file_name, '_cells.csv') || contains(file_name, 'cylinder')) 
+
+        if ~cell_file_exported
+            export_cells = true;
+        else
+            export_cells = false;
+        end
+        
+        file_name 
+        
+        [filepath,name,ext] = fileparts(file_name); 
+
+        name_no_number = name(1:end-4); 
+
+        frame_num = str2num(name(end-3:end));
+        
+        if add_mechanics && ((frame_num == 662) || (frame_num == 837))
+            export_mechanics = true 
+            export_coaptation = true
+        else 
+            export_mechanics = false
+            export_coaptation = false
+        end 
+        
+        export_aortic_vertices_cells(file_name, valve_with_reference, params, data_dir, run_inv_transform, export_cells, export_mechanics, export_coaptation); 
+        
+        if export_cells
+            cell_file_exported = true; 
+        end 
+
+    end 
+end 
+
 parfor i = 1:length(file_list)
+
+    cell_file_exported = true; 
     
     file_name = file_list(i).name; 
     
