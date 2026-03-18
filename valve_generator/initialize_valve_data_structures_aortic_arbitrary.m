5840b42b2c3470b6c942b4907098b8fc8843c5a7
alexkaiser <adkaiser@gmail.com>
2026-03-17 17:42:49 -0700

    new quad, c3 r2, comm height 1pt75, redone less med scale buckle in predicted loaded

--- valve_generator/initialize_valve_data_structures_aortic_arbitrary.m
+++ valve_generator/initialize_valve_data_structures_aortic_arbitrary.m
@@ -288,8 +288,8 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 5.95;  % circumferential 
-tension_coeffs.c_rad_dec        = 2.9;  % radial
+tension_coeffs.c_circ_dec       = 6.19;  % circumferential 
+tension_coeffs.c_rad_dec        = 2.43;  % radial
 
 tension_coeffs.c_circ_dec_annulus = 2.0;
 
