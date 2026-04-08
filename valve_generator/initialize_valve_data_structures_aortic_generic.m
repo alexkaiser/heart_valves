5affe94d992ecfacf0ebf2c4ed4adc95403c5c32
alexkaiser <adkaiser@gmail.com>
2026-04-08 09:17:06 -0700

    tri c2.8 r1.81 loaded, c annulus down more still, very little buckle

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -233,10 +233,10 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 4.93;  % circumferential 
-tension_coeffs.c_rad_dec        = 1.98;  % radial
+tension_coeffs.c_circ_dec       = 5.27;  % circumferential 
+tension_coeffs.c_rad_dec        = 2.7;  % radial
 
-tension_coeffs.c_circ_dec_annulus = 1.0;        
+tension_coeffs.c_circ_dec_annulus = 0.7;        
 
 % tension_coeffs.c_circ_dec_free_edge = 5.0;
 tension_coeffs.c_circ_dec_free_edge_percentage = 0.0;
