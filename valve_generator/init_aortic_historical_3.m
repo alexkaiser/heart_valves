f0b68bf69e8829f6f0227b03bac7e87e63a2b5a3
alexkaiser <adkaiser@gmail.com>
2026-04-14 12:09:18 -0700

    hist 3, fel/stj 1pt57 rest, gh/stj .67, raise comms more, ic distance 23

--- valve_generator/init_aortic_historical_3.m
+++ valve_generator/init_aortic_historical_3.m
@@ -235,8 +235,8 @@ tension_coeffs_lr.alpha = 1.6;   % circumferential
 tension_coeffs_lr.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs_lr.c_circ_dec       = 2.92;  % circumferential 
-tension_coeffs_lr.c_rad_dec        = 1.0;  % radial
+tension_coeffs_lr.c_circ_dec       = 2.94;  % circumferential 
+tension_coeffs_lr.c_rad_dec        = 0.9;  % radial
 tension_coeffs_lr.c_circ_dec_annulus = 1.91;
 tension_coeffs_lr.c_circ_dec_free_edge_percentage = 0.0;
 
@@ -249,8 +249,8 @@ tension_coeffs_non.alpha = 1.6;   % circumferential
 tension_coeffs_non.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs_non.c_circ_dec       = 2.961;  % circumferential 
-tension_coeffs_non.c_rad_dec        = 0.953;  % radial
+tension_coeffs_non.c_circ_dec       = 2.99;  % circumferential 
+tension_coeffs_non.c_rad_dec        = 0.835;  % radial
 tension_coeffs_non.c_circ_dec_annulus = 1.91;
 tension_coeffs_non.c_circ_dec_free_edge_percentage = 0.0;
 
