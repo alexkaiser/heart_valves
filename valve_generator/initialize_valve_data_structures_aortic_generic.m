212ede5c3b320dea7ee4b653bf2572ddb4c2b0f5
alexkaiser <adkaiser@gmail.com>
2026-05-03 20:14:50 -0700

    return trileaflet to best, d0f7d4a

--- valve_generator/initialize_valve_data_structures_aortic_generic.m
+++ valve_generator/initialize_valve_data_structures_aortic_generic.m
@@ -197,7 +197,7 @@ valve.L = 2.25;
 
 r_stj = 2.5/2; % 25 mm valve 
 r_temp = 2.5/2; % vbr radius
-hc = .765 * r_stj; % 0.5 * r_stj; 
+hc = 0.5 * r_stj; 
 h1 = 1.4 * r_stj - hc; 
 r_commissure = r_stj; 
 % place the post only if not using the full annulus geometry 
@@ -233,10 +233,10 @@ tension_coeffs.alpha = 1.6;   % circumferential
 tension_coeffs.beta  = 0.055;   % radial
 
 % decreasing tension coefficients 
-tension_coeffs.c_circ_dec       = 3;  % circumferential 
-tension_coeffs.c_rad_dec        = 0.962;  % radial
+tension_coeffs.c_circ_dec       = 3.64;  % circumferential 
+tension_coeffs.c_rad_dec        = 1.3;  % radial
 
-% tension_coeffs.c_circ_dec_annulus = 1.8;        
+tension_coeffs.c_circ_dec_annulus = 1.8;        
 
 % tension_coeffs.c_circ_dec_free_edge = 5.0;
 tension_coeffs.c_circ_dec_free_edge_percentage = 0.0;
